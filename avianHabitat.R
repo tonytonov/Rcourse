9f8b483aa3d867823b872d54b0476d70959a9fdd
Anton Antonov <tonytonov@gmail.com>
2016-03-18 09:44:18 +0300

    avianHabitat R script

--- avianHabitat.R
+++ avianHabitat.R
@@ -35,3 +35,40 @@ summary(avian$total_coverage)
 
 avian$site_name <- factor(str_replace(avian$Site, "[:digit:]+", ""))
 tapply(avian$DBHt, avian$site_name, mean)
+
+# Using dplyr 
+library(stringr)
+library(dplyr)
+options(stringsAsFactors = FALSE)
+
+# First approach
+avian <- read.csv("avianHabitat.csv")
+
+avian <- subset(avian, PDB > 0 & DBHt > 0, c("Site", "Observer", "PDB", "DBHt"))
+avian$Site <- factor(str_replace(avian$Site, "[:digit:]+", ""))
+subset(
+  aggregate(avian$DBHt, list(Site = avian$Site, Observer = avian$Observer), max),
+  x >= 5
+)
+  
+# Second approach (using pipes)
+avian <- read.csv("avianHabitat.csv")
+
+avian <-
+  avian %>% 
+  subset(PDB > 0 & DBHt > 0, c("Site", "Observer", "PDB", "DBHt")) %>% 
+  transform(Site = factor(str_replace(.$Site, "[:digit:]+", "")))
+
+aggregate(avian$DBHt, list(Site = avian$Site, Observer = avian$Observer), max) %>% 
+  subset(x >= 5)
+
+# Third approach (using both pipes and dplyr)
+avian <- read.csv("avianHabitat.csv")
+
+avian %>% 
+  filter(PDB > 0, DBHt > 0) %>% 
+  select(Site, Observer, contains("DB")) %>% 
+  mutate(Site = factor(str_replace(Site, "[:digit:]+", ""))) %>% 
+  group_by(Site, Observer) %>% 
+  summarise(MaxHt = max(DBHt)) %>% 
+  filter(MaxHt >= 5) 
